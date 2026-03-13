fc5b5639ada07476c7b7de80c6d45e831831fa67
Kroaxys <mellar08sw@gmail.com>
2026-03-13 16:09:13 +0100

    JS WORKS!!!! WOHOOOOOOOOO

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+var content;
+var input;
 
-var body = document.getElementById()
 
 document.addEventListener("DOMContentLoaded", function(){
-    
+    content = document.getElementById("content")
+    addNewLine();
+
+    document.addEventListener("keydown",function(event){
+
+        if (event.key.length==1){
+            input.textContent += event.key
+        }
+        else if(event.key=="Backspace"){
+            input.textContent = input.textContent.slice(0,-1);
+        }
+        else if(event.key=="Enter"){
+            addNewLine();
+        }
+    })
 })
 
 function addNewLine (){
-    
+    let basep = document.createElement("p")
+    input = document.createElement("p")
+    let div = document.createElement("div")
+    basep.textContent="guest@kroaxys.xyz:~$"
+    content.append(div);
+    div.append(basep);
+    div.append(input);
 }
